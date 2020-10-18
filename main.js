b010dbe395aeadbeb89254d83c02cd6436d96ecf
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-18 10:19:02 +0200

    log in if not logged in

--- main.js
+++ main.js
@@ -5,9 +5,31 @@
  * @property {string} pwd
  */
 const ids = require('./ids.json')
+const DeGiro = require('degiro-api').default
+const degiro = DeGiro.create(ids)
 
 async function start() {
+    if (await loginIfNotLoggedIn()) {
+        const jsessionId = degiro.getJSESSIONID()
+        console.log(jsessionId)
+    }  
+}
 
+/**
+ * @returns {Promise<boolean>}
+ */
+async function loginIfNotLoggedIn() {
+    return new Promise(async (resolve) => {
+        if (! degiro.isLogin()) {
+            await degiro.login()
+            if (degiro.isLogin()) {
+                resolve(true)
+            }
+            resolve(false)
+        } else {
+            resolve(true)
+        }
+    })
 }
 
 start()
