66b4b33902aaf7ccb5ffc985a9408b48723d6735
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-18 10:13:09 +0200

    ids

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('test')
+/**
+ * @typedef {Object} Ids
+ * 
+ * @property {string} username
+ * @property {string} pwd
+ */
+const ids = require('./ids.json')
+
+async function start() {
+
+}
+
+start()
